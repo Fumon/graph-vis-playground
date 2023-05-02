374705732e8390a5191016a53e97bfbab5706092
Jade Bilkey <herself@thefumon.com>
2023-05-02 14:39:49 -0400

    Eh

--- src/routes/cytotests/control-points/+page.svelte
+++ src/routes/cytotests/control-points/+page.svelte
@@ -23,51 +23,52 @@
 
 		// Edge stuff
 		cy.edges('#a-b').forEach((edge) => {
-            // let distances = [];
-            // let weights = [];
-            let points = [];
-            for(const pt in cornerPositions) {
-                points.push(cornerPositions[pt]);
-                // const {w, d} = edgePointToWD(edge, cornerPositions[pt]);
-                // distances.push(d);
-                // weights.push(w);
-            }
-            
+			// let distances = [];
+			// let weights = [];
+			let points = [];
+			for (const pt in cornerPositions) {
+				for (let index = 0; index < 1; index++) {
+					points.push(cornerPositions[pt]);
+					// const {w, d} = edgePointToWD(edge, cornerPositions[pt]);
+					// distances.push(d);
+					// weights.push(w);
+				}
+			}
 
-            const {w, d} = edgePointsToWDs(edge, points)
+			const { w, d } = edgePointsToWDs(edge, points);
 
-            console.log(`Distances: ${d}`);
-            console.log(`Weights: ${w}`);
+			console.log(`Distances: ${d}`);
+			console.log(`Weights: ${w}`);
 
 			edge.style({
 				'curve-style': 'unbundled-bezier',
 				'edge-distances': 'node-position',
 				'control-point-distances': d,
-				'control-point-weights': w,
+				'control-point-weights': w
 			});
 		});
 	});
 
 	const cornerPositions = {
-        'ctl': {x: 0, y: 0},
-		'tm': {x: 1000, y: 0},
-		'ctr': {x: 2000, y: 0},
-		'cbl': {x: 0, y: 2000},
-		'bm': {x: 1000, y: 2000},
-		'cbr': {x: 2000, y: 2000},
-    };
+		ctl: { x: 0, y: 0 },
+		tm: { x: 1000, y: 0 },
+		ctr: { x: 2000, y: 0 },
+		cbl: { x: 0, y: 2000 },
+		bm: { x: 1000, y: 2000 },
+		cbr: { x: 2000, y: 2000 }
+	};
 
 	const testElems = {
 		nodes: [
 			node('a', 50, 100),
 			node('b', 150, 100),
-            ...(() => {
-                let out = [];
-                for(const n in cornerPositions) {
-                    out.push(node(n, cornerPositions[n].x, cornerPositions[n].y))
-                }
-                return out
-            })(),
+			...(() => {
+				let out = [];
+				for (const n in cornerPositions) {
+					out.push(node(n, cornerPositions[n].x, cornerPositions[n].y));
+				}
+				return out;
+			})()
 		],
 		edges: [edge('a-b', 'a', 'b')]
 	};
